b4128e63843c8cf7ce2f6bfaef70d130d2925724
Jordan Felan <felan.jordan94@gmail.com>
2021-05-26 13:53:38 -0500

    exercise completed

---
+++ src/StringExercise.java
@@ -0,0 +1,15 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String pinkFloyd = "We dont need no education\nWe dont need no thought control";
+        System.out.println(pinkFloyd);
+
+        String parenthesis ="Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(parenthesis);
+
+        String window = "In windows, the main drive is usually C:\\";
+        System.out.println(window);
+
+        String backslash = "I can do backslashes \\, double backslashes \\\\ and the amazing triple backspace \\\\\\ ";
+        System.out.println(backslash);
+    }
+}
