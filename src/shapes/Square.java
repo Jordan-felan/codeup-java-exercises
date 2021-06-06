41446d1a0662026afc1753d64ba5e4ddec3507a1
Jordan Felan <felan.jordan94@gmail.com>
2021-06-06 14:15:14 -0500

    exercise semi completed

---
+++ src/shapes/Square.java
@@ -0,0 +1,21 @@
+package shapes;
+
+public class Square extends Rectangle{
+//    private double side;
+//
+//    public Square(double side) {
+//        super(side, side); //overrides the original length and width parameters of Rectangle
+//        this.side = side;
+//    }
+//
+//    @Override
+//    public double getPerimeter(){
+////        super.getPerimeter();
+//        return 4 * side;
+//    }
+//
+//    @Override
+//    public double getArea() {
+//        return Math.pow(side, 2);
+//    }
+}
