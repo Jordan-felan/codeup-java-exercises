0a3a5a2687201b1d22857433bd3935d42b335b99
Jordan Felan <felan.jordan94@gmail.com>
2021-06-09 09:25:05 -0500

    exercises completed

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -3,18 +3,42 @@ package grades;
 import java.util.ArrayList;
 
 public class Student {
-    private String studentName;
-    private ArrayList<Integer> grades = new ArrayList<>();
+    private String name;
 
-    public String getName() {
-        return studentName;
+    private ArrayList<Integer> grades;
+
+    public Student(String name) {
+        this.name = name;
+        this.grades = new ArrayList<>();
     }
 
-    public void addGrade(int grade) {
-
+    // returns the student's name
+    public String getName(){
+        return this.name;
     }
 
-    public double getGradeAverage() {
-        return 0;
+    //    public void setName(String name){
+//        this.name = name;
+//    }
+    // adds the given grade to the grades property
+    public void addGrade(int grade){
+        this.grades.add(grade);
     }
+    // returns the average of the students grades
+    public double getGradeAverage(){
+        double total = 0;
+        for( int grade : grades) {
+            total = total + grade;
+        }
+        return total / grades.size();
+    }
+    public static void main(String[] args) {
+        Student testStudent = new Student("Jordan");
+        testStudent.addGrade(90);
+        testStudent.addGrade(96);
+        testStudent.addGrade(85);
+        System.out.println(testStudent.getName());
+        System.out.println("The average is " + testStudent.getGradeAverage());
+    }
+
 }
