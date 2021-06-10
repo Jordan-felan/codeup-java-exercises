faa26957c5481c4ea66e01c4728e57f286018f90
Jordan Felan <felan.jordan94@gmail.com>
2021-06-10 09:01:10 -0500

    Exercise completed

--- src/util/Input.java
+++ src/util/Input.java
@@ -9,12 +9,20 @@ public class Input {
         this.scanner = new Scanner(System.in);
     }
 
-    public String getString(){
-        System.out.println("Enter a sentence");
+//    public String getString(){
+//        System.out.println("Enter a sentence");
+//        String userSentence = scanner.nextLine();
+//        System.out.println("Your sentence is: " + userSentence);
+//        return userSentence;
+//    };
+
+    public String getString(String prompt){
+        System.out.println(prompt);
         String userSentence = scanner.nextLine();
-        System.out.println("Your sentence is: " + userSentence);
+        System.out.println("You entered: " + userSentence);
         return userSentence;
     };
+
     public boolean yesNo(){
         System.out.println("Enter a yes or no?");
         String userYesNo = scanner.nextLine();
@@ -30,41 +38,62 @@ public class Input {
         return result;
     }
 
-    public int getInt(int min, int max){
-        System.out.printf("Please enter a number between %d and %d.%n", min, max);
-        int userNum = scanner.nextInt();
-        if(userNum <= min || userNum >= max){
-            System.out.println("Oops, that wasnt within the range!");
-            getInt(min, max);
-        } else {
-            System.out.printf("Thank you! Your number %d is between the range!%n", userNum);
-        }
-        return userNum;
-    }
+//    public int getInt(int min, int max){
+//        System.out.printf("Please enter a number between %d and %d.%n", min, max);
+//        int userNum = scanner.nextInt();
+//        if(userNum <= min || userNum >= max){
+//            System.out.println("Oops, that wasnt within the range!");
+//            getInt(min, max);
+//        } else {
+//            System.out.printf("Thank you! Your number %d is between the range!%n", userNum);
+//        }
+//        return userNum;
+//    }
+//
+//    public int getInt(){
+//        System.out.println("Please enter a number");
+//        int userNum = scanner.nextInt();
+//        return userNum;
+//
+//    }
+
+//    public double getDouble(double min, double max){
+//        System.out.printf("Please enter a number between %f and %f.%n It may include a decimal! %n", min, max);
+//        double userDouble = scanner.nextDouble();
+//        if(userDouble < min || userDouble > max){
+//            System.out.println("Oops, that wasn't within the range!");
+//            getDouble(min, max);
+//        } else {
+//            System.out.printf("Thank you! Your number %f is between the range!%n", userDouble);
+//        }
+//        return userDouble;
+//    }
+//
+//    public double getDouble(){
+//        System.out.println("Please enter a decimal number!");
+//        double userDouble = scanner.nextDouble();
+//        return userDouble;
+//    };
+
 
     public int getInt(){
-        System.out.println("Please enter a number");
-        int userNum = scanner.nextInt();
-        return userNum;
 
-    }
-
-    public double getDouble(double min, double max){
-        System.out.printf("Please enter a number between %f and %f.%n It may include a decimal! %n", min, max);
-        double userDouble = scanner.nextDouble();
-        if(userDouble < min || userDouble > max){
-            System.out.println("Oops, that wasn't within the range!");
-            getDouble(min, max);
-        } else {
-            System.out.printf("Thank you! Your number %f is between the range!%n", userDouble);
+        try {
+            Integer.valueOf(getString("Enter a number!"));
+        } catch (NumberFormatException nfe){
+            System.out.println("Uh oh, " + nfe.getMessage() + " there is no integer!");
         }
-        return userDouble;
-    }
+        return 0;
+
+    };
 
     public double getDouble(){
-        System.out.println("Please enter a decimal number!");
-        double userDouble = scanner.nextDouble();
-        return userDouble;
+        try {
+            Double.valueOf(getString("Enter a decimal Number"));
+        } catch (NumberFormatException nfe){
+            System.out.println("Uh oh, " + nfe.getMessage() + " that is not a decimal number!!");
+        }
+        return 0;
     };
 
 
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -4,12 +4,12 @@ public class InputTest {
 
     public static void main(String[] args) {
         Input input2 = new Input();
-        System.out.println(input2.getString());
-        System.out.println(input2.yesNo());
-        System.out.println(input2.getInt(1,10));
+//        System.out.println(input2.getString());
+//        System.out.println(input2.yesNo());
+//        System.out.println(input2.getInt(1,10));
         System.out.println(input2.getInt());
-        System.out.println(input2.getDouble(1.99, 99.99));
-        System.out.println(input2.getDouble());
+//        System.out.println(input2.getDouble(1.99, 99.99));
+//        System.out.println(input2.getDouble());
 
     }
 }
