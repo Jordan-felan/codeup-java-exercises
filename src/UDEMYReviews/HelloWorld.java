567151be380dac894a3d3e54402196a1ab414ba1
Jordan Felan <felan.jordan94@gmail.com>
2021-10-02 18:37:48 -0500

    started next part of udemy java review

--- src/UDEMYReviews/HelloWorld.java
+++ src/UDEMYReviews/HelloWorld.java
@@ -4,5 +4,13 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello, World");
+
+        int myFavoriteNumber = 74;
+        System.out.println(myFavoriteNumber);
+        String myString = "Dude, wheres my car?";
+        System.out.println(myString);
+        float myNumber = 3.14F;
+        float myNumber2 = (float) 3.14;
+        System.out.println(myNumber);
     }
 }
