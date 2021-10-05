8ca439181d533789effd8f2610862966f955ef65
Jordan Felan <felan.jordan94@gmail.com>
2021-10-04 21:55:47 -0500

    heap dump

---
+++ src/UDEMYReviews/Variables.java
@@ -0,0 +1,12 @@
+package UDEMYReviews;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        String helloWorld = "Hello, world";
+        int myFirstNumber = 5;
+
+       System.out.println(helloWorld);
+       System.out.println(myFirstNumber);
+    }
+}
