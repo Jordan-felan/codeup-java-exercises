88f4e8d564c81b8b9d6a1ddb8bb6d7253a3b2d5b
Jordan Felan <felan.jordan94@gmail.com>
2021-10-05 00:34:29 -0500

    started recap of variables and operators

--- src/UDEMYReviews/Variables.java
+++ src/UDEMYReviews/Variables.java
@@ -5,6 +5,11 @@ public class Variables {
     public static void main(String[] args) {
         String helloWorld = "Hello, world";
         int myFirstNumber = 5;
+        int myFirstAddition = 5 + 10;
+        int myFirstSubtraction = 5 - 10;
+        int myFirstMultiplication = 5 * 10;
+        int myFirstDivision = 5 / 10;
+
 
        System.out.println(helloWorld);
        System.out.println(myFirstNumber);
