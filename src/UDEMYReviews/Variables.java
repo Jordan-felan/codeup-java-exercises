b9f3f9a6437a2e6401eed2b8250e32457e418f13
Jordan Felan <felan.jordan94@gmail.com>
2021-10-07 20:43:08 -0500

    reviewing variables again in udemy

--- src/UDEMYReviews/Variables.java
+++ src/UDEMYReviews/Variables.java
@@ -9,9 +9,26 @@ public class Variables {
         int myFirstSubtraction = 5 - 10;
         int myFirstMultiplication = 5 * 10;
         int myFirstDivision = 5 / 10;
+        System.out.println(helloWorld);
+        System.out.println(myFirstNumber);
 
 
-       System.out.println(helloWorld);
-       System.out.println(myFirstNumber);
+        int x = 5;
+//        System.out.println(x++);
+        System.out.println(x);
+        System.out.println(++x);
+        System.out.println(x);
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+        int three = (int) o;
+//        int three = (int) "three";
+//        int x = 4;
+//        System.out.println(x += 5);
+//        int x = 3;
+//        int y = 4;
+//        System.out.println(y*=x);
+
+
+
     }
 }
