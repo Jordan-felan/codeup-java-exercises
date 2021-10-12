66a1e5a2ecdcdf1a4c9739407d111a56c3df8546
Jordan Felan <felan.jordan94@gmail.com>
2021-10-12 12:00:00 -0500

    commit message

--- src/UDEMYReviews/PrimitiveTypes.java
+++ src/UDEMYReviews/PrimitiveTypes.java
@@ -32,7 +32,7 @@ public class PrimitiveTypes {
         byte myMaxByteValue = Byte.MAX_VALUE;
         System.out.println("Byte Minimum Value = " + myMinByteValue);
         System.out.println("Byte Maximum Value = " + myMaxByteValue);
-
+//
 
     }
 }
