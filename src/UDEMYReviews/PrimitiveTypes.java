d392d72dab299338bd62cc5f8bf64c93f3e2466d
Jordan Felan <felan.jordan94@gmail.com>
2021-10-11 19:42:43 -0500

    started covering primitive types again. bytes, ints, shorts and longs

---
+++ src/UDEMYReviews/PrimitiveTypes.java
@@ -0,0 +1,33 @@
+package UDEMYReviews;
+
+public class PrimitiveTypes {
+
+    public static void main(String[] args) {
+
+        //primitive types
+//        boolean
+//        byte
+//        char
+//        short
+//        int
+//        long
+//        float
+
+
+        //INTEGERS
+
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+        //ints range is -2,147,483,648 to 2,147,483,647
+
+
+        //BYTES
+
+
+
+    }
+}
--- src/UDEMYReviews/Variables.java
+++ src/UDEMYReviews/Variables.java
@@ -4,13 +4,17 @@ public class Variables {
 
     public static void main(String[] args) {
         String helloWorld = "Hello, world";
-        int myFirstNumber = 5;
+        int myFirstNumber = (10 + 5) + (2 * 10);
         int myFirstAddition = 5 + 10;
         int myFirstSubtraction = 5 - 10;
         int myFirstMultiplication = 5 * 10;
         int myFirstDivision = 5 / 10;
         System.out.println(helloWorld);
         System.out.println(myFirstNumber);
+        System.out.println(myFirstAddition);
+        System.out.println(myFirstSubtraction);
+        System.out.println(myFirstMultiplication);
+        System.out.println(myFirstDivision);
 
 
         int x = 5;
@@ -18,15 +22,31 @@ public class Variables {
         System.out.println(x);
         System.out.println(++x);
         System.out.println(x);
-        String theNumberThree = "three";
-        Object o = theNumberThree;
-        int three = (int) o;
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
 //        int three = (int) "three";
 //        int x = 4;
 //        System.out.println(x += 5);
 //        int x = 3;
 //        int y = 4;
 //        System.out.println(y*=x);
+
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myTotal);
+        System.out.println(myLastOne);
+
+        //primitive types
+//        boolean
+//        byte
+//        char
+//        short
+//        int
+//        long
+//        float
 
 
 
